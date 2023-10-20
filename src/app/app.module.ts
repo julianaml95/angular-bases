1fffbc6aff9314f760713004ce66efb99f4a800e
Juliana Mora <jmora216@unicauca.edu.co>
2023-10-20 10:17:28 -0500

    Fin de sección 4

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { CounterModule } from './counter/components/counter/counter.module';
+
 import { AppComponent } from './app.component';
+import { HeroesModule } from './heroes/hero.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    CounterModule,
+    HeroesModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
