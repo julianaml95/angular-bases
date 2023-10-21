ca5f6f0cfea36adc67f8f66b884455bfed4d1e4e
Juliana Mora <jmora216@unicauca.edu.co>
2023-10-21 17:24:24 -0500

    Primer commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { CounterModule } from './counter/components/counter/counter.module';
+import { DbzModule } from './dbz/dbz.module';
+import { HeroesModule } from './heroes/hero.module';
+
 
 import { AppComponent } from './app.component';
-import { HeroesModule } from './heroes/hero.module';
 
 @NgModule({
   declarations: [
@@ -14,6 +16,7 @@ import { HeroesModule } from './heroes/hero.module';
     BrowserModule,
     CounterModule,
     HeroesModule,
+    DbzModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
